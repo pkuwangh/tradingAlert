6e303c49762a6a6d3ff37cb94132ad5044686354
Hao Wang <pkuwangh@gmail.com>
2018-10-27 00:33:32 -0500

    [data] chrome driver class to download raw data; add option chain example

--- data_retrieve/webpage.py
+++ data_retrieve/webpage.py
@@ -1,28 +1,54 @@
 #!/home/wangh/anaconda3/bin/python
 
-from selenium import webdriver  
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from PIL import Image
-from io import BytesIO
+from datetime import datetime
+import logging
+logger = logging.getLogger(__name__)
 
-CHROME_PATH = '/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = '/home/wangh/local/bin/chromedriver'
-WINDOW_SIZE = "1920,3240"
+class ChromeDriver:
+    # class members
+    binary_path = '/usr/bin/google-chrome'
+    driver_path = '/usr/local/bin/chromedriver'
 
-chrome_options = Options()  
-chrome_options.add_argument("--headless")  
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-chrome_options.binary_location = CHROME_PATH
+    def __init__(self, width=1920, height=3240):
+        self.chrome_options = Options()
+        self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--window-size=1920,3240')
+        self.chrome_options.binary_location = ChromeDriver.binary_path
+        self.driver = webdriver.Chrome(executable_path=ChromeDriver.driver_path,
+                                       options=self.chrome_options)
 
-def make_screenshot(url):
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
-    driver.get(url)
-    driver.save_screenshot('full.png')
-    element = driver.find_element_by_id('main-content-column')
-    with open('data.txt', 'w') as fout:
-        fout.write(element.text)
-    driver.close()
+    def download_data(self, url, element_id=None, class_name=None, outfile=None):
+        logger.info('%s download data from %s'
+                % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), url))
+        # access the url
+        self.driver.get(url)
+        # get the target element
+        if element_id:
+            element = self.driver.find_element_by_id(element_id)
+        elif class_name:
+            element = self.driver.find_element_by_class_name(class_name)
+        else:
+            logger.error('Do not know how to find element')
+            raise
+        # output & return
+        if outfile:
+            with open(outfile, 'w') as fout:
+                fout.write(element.text)
+        return element.text
+
+    def close(self):
+        self.driver.close()
 
 if __name__ == '__main__':
-    make_screenshot('https://www.barchart.com/options/unusual-activity')
+    import sys
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    chrome_driver = ChromeDriver(height=3240)
+    url = 'https://finance.yahoo.com/quote/MSFT/options?date=1542326400';
+    eid = 'Col1-1-OptionContracts-Proxy'
+    data = chrome_driver.download_data(url=url, element_id=eid)
+    print (data)
+    chrome_driver.close()
+
