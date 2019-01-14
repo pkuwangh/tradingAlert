65c94bcf5ccbb8069b88104cafcebb817499aaae
Hao Wang <pkuwangh@gmail.com>
2019-01-14 16:42:24 -0600

    [comm] modulized MailMan

--- utils/send_email.py
+++ utils/send_email.py
@@ -13,32 +13,51 @@ root_dir = '/'.join(os.path.abspath(os.path.dirname(__file__)).split('/')[:-1])
 sys.path.append(root_dir)
 from utils.datetime_string import *
 
+class MailMan:
+    def __init__(self):
+        self.__status_good = True
+        try:
+            self.msg = MIMEMultipart()
+            import json
+            # sender
+            sender_json = os.path.join(root_dir, 'email', 'sender.json')
+            with open(sender_json, 'r') as fp:
+                sender_dict = json.load(fp)
+            self.__sender = list(sender_dict.keys())[-1]
+            self.__password = sender_dict[self.__sender]
+            self.msg['From'] = self.__sender
+            # receivers
+            receiver_json = os.path.join(root_dir, 'email', 'receiver.json')
+            with open(receiver_json, 'r') as fp:
+                receiver_dict = json.load(fp)
+            receivers = [v for v in receiver_dict.values()]
+            self.msg['To'] = ','.join(receivers)
+        except Exception as e:
+            logger.error('error when initing MailMan: %s' % (e))
+            self.__status_good = False
+
+    def send(self, subject, content):
+        if self.__status_good:
+            self.msg['Subject'] = subject
+            main_text = MIMEText(content, 'plain')
+            self.msg.attach(main_text)
+            # send
+            try:
+                server = smtplib.SMTP('{}:{}'.format('smtp.gmail.com', 587))
+                server.starttls()
+                server.login(self.__sender, self.__password)
+                server.send_message(self.msg)
+                server.quit()
+                logger.info('sent email to subscribers')
+            except Exception as e:
+                logger.error('error when sending email: %s' % (e))
+        else:
+            pass
+
 if __name__ == '__main__':
-    # create the container email message
-    msg = MIMEMultipart();
-    msg['Subject'] = 'Option activity on %s' % get_datetime_str(datetime.datetime.now())
-    import json
-    # sender
-    sender_json = os.path.join(root_dir, 'email', 'sender.json')
-    with open(sender_json, 'r') as fp:
-        sender_dict = json.load(fp)
-    sender = list(sender_dict.keys())[-1]
-    password = sender_dict[sender]
-    msg['From'] = sender
-    # receivers
-    receiver_json = os.path.join(root_dir, 'email', 'receiver.json')
-    with open(receiver_json, 'r') as fp:
-        receiver_dict = json.load(fp)
-    receivers = [v for v in receiver_dict.values()]
-    msg['To'] = ','.join(receivers)
-    # main content
+    subject = 'Option activity on %s' % get_datetime_str(datetime.datetime.now())
     with open(os.path.join(root_dir, 'notes.txt'), 'r') as fp:
         text = fp.read()
-    main_text = MIMEText(text, 'plain')
-    msg.attach(main_text)
-    # send
-    server = smtplib.SMTP('{}:{}'.format('smtp.gmail.com', 587))
-    server.starttls()
-    server.login(sender, password)
-    server.send_message(msg)
-    server.quit()
+    # create a MailMan object
+    mail_man = MailMan()
+    mail_man.send(subject=subject, content=text)
